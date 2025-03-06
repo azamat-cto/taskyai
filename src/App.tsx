4810a7abcbea9acf2b17fa10bc49ee134fd0255a
azamat.xabibullayev <azamatkhabibullaevcto@gmail.com>
2025-03-06 17:51:11 +0500

    Add inter google font

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { Button } from "./components/ui/button";
-
 function App() {
     return (
         <>
-            <div className="h-screen flex items-center justify-center">
-                <Button>Shadcn Button</Button>
-            </div>
+            <div>App</div>
         </>
     );
 }
